8735d18561871602b3b6b68ca8c80fba0597ce77
Vikram Pawar <pvikram035@gmail.com>
2024-10-26 01:01:00 -0400

    Fixed bug

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println("Please input your guess.");
+    println!("Please input your guess.");
 
     let mut guess = String::new();
 
