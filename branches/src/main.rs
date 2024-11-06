5debeecb516bb4694a1d94ba553810d339b23fd8
Vikram Pawar <pvikram035@gmail.com>
2024-11-05 19:22:11 -0500

    Updated branches

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
     let number = 3;
 
-    if number < 5 {
+    if number != 0 {
         println!("condition was true");
-    } else {
-        println!("condition was false");
-    }
+    } 
 }
