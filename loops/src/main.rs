e63555bac227bf02283ca613cdc261fd8b9b75c9
Vikram Pawar <pvikram035@gmail.com>
2024-11-05 20:58:49 -0500

    loop labels

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,13 +1,25 @@
 fn main() {
-    let mut counter = 0;
 
-    let result = loop {
-        counter += 1;
+    let mut count = 0;
 
-        if counter == 10 {
-            break counter * 2;
+    'counting_up: loop {
+        println!("count = {count}");
+
+
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
         }
-    };
 
-    println!("The result is {result}")
+        count += 1;
+    }
+    println!("End count = {count}");
 }
