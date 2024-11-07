1a1b281bad467e83e4a282050337b5c0221dd304
Vikram Pawar <pvikram035@gmail.com>
2024-11-07 00:35:49 -0500

    Added ownership

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,3 @@
 fn main() {
-    let y = {
-        let x = 3;
-        x + 1
-    };
-
-    println!("The value of y is; {y}");
+    print_labeled_measu
 }
---
+++ ownership/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+
+    // Variable Scope
+    {
+        let s = "hello"; // s is valid from this point forward
+
+        // do stuff with s
+    }                   // this scop is now over, and s is no longer valid
+}
